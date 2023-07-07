3f7b2b389d100eefe7b5de065bd9a0b38f529cd6
Abhijeet Singh Khangarot <abhijeet40308@gmail.com>
2023-07-08 01:06:18 +0530

    add

---
+++ src/components/prabhu-walls/PrivacyPolicy.tsx
@@ -0,0 +1,129 @@
+export const PrivacyPolicy = () => {
+  return (
+    <>
+      <div>
+        Privacy Policy Effective date: Jul 08, 2023.
+        <br />
+        <br />
+        We, Prabhu Wallpapers is a wallpapers application offered by Cross
+        Platform Apps. Prabhu Wallpapers attach great importance to users
+        personal privacy. We strongly advise you to read this policy carefully
+        to fully understand how we use and protect your interests. If you have
+        any questions, please contact us at abhijeet40308@gmail.com.
+        <br />
+        <br />
+        Disclaimer: Please note that we are not affiliated with or authorized by
+        any government institutions. Our app is entirely developed by private
+        groups.
+        <br />
+        <br />
+        1. The information you give to us or give us permission to obtain it •
+        <br />
+        <br />
+        Storage permission
+        <br />
+        When you want to save the wallpapers to your phone, we may need storage
+        permission to read and save your work on your device.
+        <br />
+        <br />
+        2. The technical information we get, or the third party may get when you
+        use our app
+        <br />
+        <br />
+        * Log data When you emailed us, we will receive the log data that the
+        system automatically sent to us. This log data includes your name, email
+        address, device information, location information and your response.
+        <br />* Usage data We may work with the third party such as Google
+        Firebase to collect information about your activity on our app, like
+        service-related and performance information. This includes information
+        about how you use our app, the time, frequency, and duration of your
+        activities. Based on the information we collect from your activity, we
+        could analyze usage trends for the services to improve our service.
+        <br />
+        <br /> 3. What we do with the information we collect
+        <br />* To serve you with our app.
+        <br />* To improve user experience. <br />* To communicate with you.{' '}
+        <br />* To comply with your requests.
+        <br />
+        <br /> 4. The rights you have about your information We respect the
+        rights you have about your information. Under the applicable law in your
+        jurisdiction, you may have one or more rights we list below, unless
+        otherwise specified (the California residents or the residents of the
+        EEA please refer to the specific section). <br /> *The right to know,
+        what information we collect from you, how we use it and who we shared
+        with. • The right to correct, if you found that your information that we
+        collect is inaccurate or incomplete. <br /> *The right to delete. <br />{' '}
+        *The right to revoke, if you don’t want us to access your device. <br />{' '}
+        *The right to opt out, if you don’t want to receive personalized ads (if
+        we display the personalized ads on our app). You could exercise your
+        rights by emailing us at abhijeet40308@gmail.com or by yourself through
+        our app service or the setting on your device.
+        <br />
+        <br />
+        5.How we share, transfer and disclose your information <br /> *How we
+        share We will not share your information except for the specified
+        situations below. 1）To comply with your request <br /> *How we transfer
+        We will not transfer your information to any company, organization or
+        individual, except for the requirements of laws and regulations,
+        mandatory administrative law enforcement, judicial or regulatory
+        requirements. <br /> *How we disclose We will not disclose your
+        information except for the specified situations below. 1）To comply with
+        your request
+        <br />
+        <br />
+        6.How long we store your information Our app does not store any
+        information from you.
+        <br />
+        <br />
+        7.Safeguards We will try our best to secure your information to keep
+        them private and complete. We protect them by the information security
+        and access policies that limit unauthorized access to our systems, and
+        technological protection measures such as encryption that ensure
+        confidentiality of your information during analyzing or transmission.
+        However, please understand that any transmission of information via
+        internet is not 100% secure. In other word, we can’t guarantee that we
+        can protect your information any loss, misuse, unauthorized access,
+        disclosure through our app with error-free. <br />
+        <br />
+        8.Policy on children’s information Our app is design for user over 13
+        (or such applicable age of majority under relevant local laws of the
+        user) [“specific age”]. If you are the user who is under the specific
+        age and need to handle some permission consent from our app, please do
+        it with the help of your parents or the guardian. If we are notified
+        that we have accidentally collected children’s information in a manner
+        inconsistent with the laws, we will delete it as soon as we can.
+        <br />
+        <br />
+        9.For California residents The California Consumer Privacy Act (CCPA)
+        requires us to communicate information about rights California residents
+        have under California law. You may exercise the following rights: <br />{' '}
+        *Right to know and access. You may submit a verifiable request for
+        information regarding the: (1) categories of Personal Information we
+        collect, use, or share; (2) purposes for which categories of Personal
+        Information are collected or used by us; (3) categories of sources from
+        which we collect Personal Information; and (4) specific pieces of
+        Personal Information we have collected about you. <br /> *Right to equal
+        service. We will not discriminate against you if you exercise your
+        privacy rights. <br /> *Right to delete. You may submit a verifiable
+        request to delete personal information about you that we have collected.{' '}
+        <br />
+        <br />
+        10.For the residents of the EEA The General Data Protection Regulation
+        (“GDPR”) requires us to communicate information about rights the
+        residents of the EEA. <br /> *Right of access by the data subject.{' '}
+        <br /> *Right to rectification. <br /> *Right to erasure (‘right to be
+        forgotten’). <br /> *Right to restriction of processing. <br /> *Right
+        to data portability. <br /> *Right to object. <br />
+        <br />
+        11.Changes to our policy We may change this policy from time to time and
+        if we do. If you continue to use our app after those changes are in
+        effect, you agree to the new policy. If the changes are significant, we
+        may display a popup to notice or get your consent, as required by law.{' '}
+        <br />
+        <br />
+        12.Contact us If you have any questions on our policy, please feel free
+        to contact us by emailing at: abhijeet40308@gmail.com.
+      </div>
+    </>
+  );
+};
